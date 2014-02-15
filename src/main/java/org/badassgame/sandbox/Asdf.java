f0945e775dde7dc8510f8eb070a480745c7ec55e
Brandon Charles Estle <bcestle@gmail.com>
2014-02-15 23:30:08 +0000

    created unit test target and refactored some targets

--- src/main/java/org/badassgame/sandbox/Asdf.java
+++ src/main/java/org/badassgame/sandbox/Asdf.java
@@ -1,14 +1,11 @@
 package org.badassgame.sandbox;
 
-import org.badassgame.data.Entity;
-
 /**
  * Created with IntelliJ IDEA.
  * User: Brandon
  * Date: 2/15/14
- * Time: 4:37 AM
+ * Time: 5:50 PM
  * To change this template use File | Settings | File Templates.
  */
-public interface Test {
-    Entity doSomething();
+public class Asdf {
 }
---
+++ src/test/java/org/badassgame/sandbox/JunitConfigurationTest.java
@@ -0,0 +1,39 @@
+package org.badassgame.sandbox;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Brandon
+ * Date: 2/15/14
+ * Time: 3:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JunitConfigurationTest {
+
+    @Before
+    public void createFixture() {
+        assertTrue(true);
+        System.out.println("creating a fixture");
+    }
+
+    @Test
+    public void basicUnitTest() {
+        assertTrue(true);
+        System.out.println("running unit test...");
+        System.out.println(new Asdf());
+    }
+
+    @Test
+    public void basicUnitTestTwo() {
+        assertTrue(true);
+        System.out.println("running unit test two...");
+//        System.out.println(new Asdf());
+        try {
+            Class.forName("org.badassgame.data.Entity");
+        } catch (ClassNotFoundException e) {
+            fail("could not find class entity");
+        }
+    }
+}
