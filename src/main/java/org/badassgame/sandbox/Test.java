f54abc536ad20ccb03fdf7e77e1ea3843b140579
Brandon Charles Estle <bcestle@gmail.com>
2014-02-15 09:42:28 +0000

    sandbox entity publish test

---
+++ src/main/java/org/badassgame/sandbox/Test.java
@@ -0,0 +1,14 @@
+package org.badassgame.sandbox;
+
+import org.badassgame.data.Entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Brandon
+ * Date: 2/15/14
+ * Time: 4:37 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Test {
+    Entity doSomething();
+}
