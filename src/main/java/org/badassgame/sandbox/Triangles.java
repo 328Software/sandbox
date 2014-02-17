83ab0d03907a7ae710a7582b04426952b404019b
Alexander Richard Smith <alumman@gmail.com>
2014-02-17 05:34:18 +0000

    TRIANGLESSS

--- src/main/java/org/badassgame/sandbox/Triangles.java
+++ src/main/java/org/badassgame/sandbox/Triangles.java
@@ -9,6 +9,7 @@ package org.badassgame.sandbox;
  */
 
 
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
@@ -17,28 +18,31 @@ import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.GLU;
 
-
 /**
- * NeHe Lesson 03 - Adding Color
+ * NeHe Lesson 05 - 3D Shapes
  *
  * @author Tim Biedert
  * @author kappaOne
  */
 public class Triangles {
 
-    String windowTitle = "NeHe Lesson 03 - Adding Color";
+    String windowTitle = "NeHe Lesson 05 - 3D Shapes";
     public boolean closeRequested = false;
+
+    long lastFrameTime; // used to calculate delta
+
+    float triangleAngle; // Angle of rotation for the triangles
+    float quadAngle; // Angle of rotation for the quads
 
     public void run() {
 
         createWindow();
-
+        getDelta(); // Initialise delta timer
         initGL();
 
-        // main loop
         while (!closeRequested) {
             pollInput();
-            updateLogic();
+            updateLogic(getDelta());
             renderGL();
 
             Display.update();
@@ -47,9 +51,6 @@ public class Triangles {
         cleanup();
     }
 
-    /**
-     * Initialise OpenGL
-     */
     private void initGL() {
 
 		/* OpenGL */
@@ -72,45 +73,100 @@ public class Triangles {
 
     }
 
-    /**
-     * Update Logic
-     */
-    private void updateLogic() {
-
+    private void updateLogic(int delta) {
+        triangleAngle += 0.1f * delta; // Increase The Rotation Variable For The Triangles
+        quadAngle -= 0.05f * delta; // Decrease The Rotation Variable For The Quads
     }
+
 
     private void renderGL() {
 
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT); // Clear The Screen And The Depth Buffer
         GL11.glLoadIdentity(); // Reset The View
+        GL11.glTranslatef(-1.5f, 0.0f, -6.0f); // Move Left And Into The Screen
 
-        GL11.glTranslatef(-1.5f, 0.0f, -6.0f); // Move Left 1.5 Units And Into The Screen 6.0
+        GL11.glRotatef(triangleAngle, 0.0f, 1.0f, 0.0f); // Rotate The Pyramid On It's Y Axis
 
-        GL11.glBegin(GL11.GL_TRIANGLES); // Drawing Using Triangles
-        GL11.glColor3f(1.0f, 0.0f, 0.0f); // Set The Color To Red
-        GL11.glVertex3f(0.0f, 1.0f, 0.0f); // Top
+        GL11.glBegin(GL11.GL_TRIANGLES); // Start Drawing The Pyramid
+        GL11.glColor3f(1.0f, 0.0f, 0.0f); // Red
+        GL11.glVertex3f(0.0f, 1.0f, 0.0f); // Top Of Triangle (Front)
+        GL11.glColor3f(0.0f, 1.0f, 0.0f); // Green
+        GL11.glVertex3f(-1.0f, -1.0f, 1.0f); // Left Of Triangle (Front)
+        GL11.glColor3f(0.0f, 0.0f, 1.0f); // Blue
+        GL11.glVertex3f(1.0f, -1.0f, 1.0f); // Right Of Triangle (Front)
+
+        GL11.glColor3f(1.0f, 0.0f, 0.0f); // Red
+        GL11.glVertex3f(0.0f, 1.0f, 0.0f); // Top Of Triangle (Right)
+        GL11.glColor3f(0.0f, 0.0f, 1.0f); // Blue
+        GL11.glVertex3f(1.0f, -1.0f, 1.0f); // Left Of Triangle (Right)
+        GL11.glColor3f(0.0f, 1.0f, 0.0f); // Green
+        GL11.glVertex3f(1.0f, -1.0f, -1.0f); // Right Of Triangle (Right)
+
+        GL11.glColor3f(1.0f, 0.0f, 0.0f); // Red
+        GL11.glVertex3f(0.0f, 1.0f, 0.0f); // Top Of Triangle (Back)
+        GL11.glColor3f(0.0f, 1.0f, 0.0f); // Green
+        GL11.glVertex3f(1.0f, -1.0f, -1.0f); // Left Of Triangle (Back)
+        GL11.glColor3f(0.0f, 0.0f, 1.0f); // Blue
+        GL11.glVertex3f(-1.0f, -1.0f, -1.0f); // Right Of Triangle (Back)
+
+        GL11.glColor3f(1.0f, 0.0f, 0.0f); // Red
+        GL11.glVertex3f(0.0f, 1.0f, 0.0f); // Top Of Triangle (Left)
+        GL11.glColor3f(0.0f, 0.0f, 1.0f); // Blue
+        GL11.glVertex3f(-1.0f, -1.0f, -1.0f); // Left Of Triangle (Left)
+        GL11.glColor3f(0.0f, 1.0f, 0.0f); // Green
+        GL11.glVertex3f(-1.0f, -1.0f, 1.0f); // Right Of Triangle (Left)
+        GL11.glEnd(); // Done Drawing The Pyramid
+
+        GL11.glLoadIdentity(); // Reset The View
+        GL11.glTranslatef(1.5f, 0.0f, -7.0f); // Move Right And Into The Screen
+
+        GL11.glRotatef(quadAngle, 1.0f, 1.0f, 1.0f); // Rotate The Cube On X, Y & Z
+
+        GL11.glBegin(GL11.GL_QUADS); // Start Drawing The Cube
         GL11.glColor3f(0.0f, 1.0f, 0.0f); // Set The Color To Green
-        GL11.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
+        GL11.glVertex3f(1.0f, 1.0f, -1.0f); // Top Right Of The Quad (Top)
+        GL11.glVertex3f(-1.0f, 1.0f, -1.0f); // Top Left Of The Quad (Top)
+        GL11.glVertex3f(-1.0f, 1.0f, 1.0f); // Bottom Left Of The Quad (Top)
+        GL11.glVertex3f(1.0f, 1.0f, 1.0f); // Bottom Right Of The Quad (Top)
+
+        GL11.glColor3f(1.0f, 0.5f, 0.0f); // Set The Color To Orange
+        GL11.glVertex3f(1.0f, -1.0f, 1.0f); // Top Right Of The Quad (Bottom)
+        GL11.glVertex3f(-1.0f, -1.0f, 1.0f); // Top Left Of The Quad (Bottom)
+        GL11.glVertex3f(-1.0f, -1.0f, -1.0f); // Bottom Left Of The Quad (Bottom)
+        GL11.glVertex3f(1.0f, -1.0f, -1.0f); // Bottom Right Of The Quad (Bottom)
+
+        GL11.glColor3f(1.0f, 0.0f, 0.0f); // Set The Color To Red
+        GL11.glVertex3f(1.0f, 1.0f, 1.0f); // Top Right Of The Quad (Front)
+        GL11.glVertex3f(-1.0f, 1.0f, 1.0f); // Top Left Of The Quad (Front)
+        GL11.glVertex3f(-1.0f, -1.0f, 1.0f); // Bottom Left Of The Quad (Front)
+        GL11.glVertex3f(1.0f, -1.0f, 1.0f); // Bottom Right Of The Quad (Front)
+
+        GL11.glColor3f(1.0f, 1.0f, 0.0f); // Set The Color To Yellow
+        GL11.glVertex3f(1.0f, -1.0f, -1.0f); // Bottom Left Of The Quad (Back)
+        GL11.glVertex3f(-1.0f, -1.0f, -1.0f); // Bottom Right Of The Quad (Back)
+        GL11.glVertex3f(-1.0f, 1.0f, -1.0f); // Top Right Of The Quad (Back)
+        GL11.glVertex3f(1.0f, 1.0f, -1.0f); // Top Left Of The Quad (Back)
+
         GL11.glColor3f(0.0f, 0.0f, 1.0f); // Set The Color To Blue
-        GL11.glVertex3f(1.0f, -1.0f, 0.0f); // Bottom Right
-        GL11.glEnd(); // Finished Drawing The Triangle
+        GL11.glVertex3f(-1.0f, 1.0f, 1.0f); // Top Right Of The Quad (Left)
+        GL11.glVertex3f(-1.0f, 1.0f, -1.0f); // Top Left Of The Quad (Left)
+        GL11.glVertex3f(-1.0f, -1.0f, -1.0f); // Bottom Left Of The Quad (Left)
+        GL11.glVertex3f(-1.0f, -1.0f, 1.0f); // Bottom Right Of The Quad (Left)
 
-        GL11.glTranslatef(3.0f, 0.0f, 0.0f); // Move Right 3 Units
-
-        GL11.glColor3f(0.5f, 0.5f, 1.0f); // Set The Color To Blue One Time Only
-        GL11.glBegin(GL11.GL_QUADS); // Draw A Quad
-        GL11.glVertex3f(-1.0f, 1.0f, 0.0f); // Top Left
-        GL11.glVertex3f(1.0f, 1.0f, 0.0f); // Top Right
-        GL11.glVertex3f(1.0f, -1.0f, 0.0f); // Bottom Right
-        GL11.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
+        GL11.glColor3f(1.0f, 0.0f, 1.0f); // Set The Color To Violet
+        GL11.glVertex3f(1.0f, 1.0f, -1.0f); // Top Right Of The Quad (Right)
+        GL11.glVertex3f(1.0f, 1.0f, 1.0f); // Top Left Of The Quad (Right)
+        GL11.glVertex3f(1.0f, -1.0f, 1.0f); // Bottom Left Of The Quad (Right)
+        GL11.glVertex3f(1.0f, -1.0f, -1.0f); // Bottom Right Of The Quad (Right)
         GL11.glEnd(); // Done Drawing The Quad
+
     }
 
-
-
-
-
+    /**
+     * Poll Input
+     */
     public void pollInput() {
+
         // scroll through key events
         while (Keyboard.next()) {
             if (Keyboard.getEventKeyState()) {
@@ -124,6 +180,19 @@ public class Triangles {
         }
     }
 
+    /**
+     * Calculate how many milliseconds have passed
+     * since last frame.
+     *
+     * @return milliseconds passed since last frame
+     */
+    public int getDelta() {
+        long time = (Sys.getTime() * 1000) / Sys.getTimerResolution();
+        int delta = (int) (time - lastFrameTime);
+        lastFrameTime = time;
+
+        return delta;
+    }
 
     private void createWindow() {
         try {
