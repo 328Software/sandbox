2bcd5a9972642d672d0b19773be01d7d20a4c760
Brandon Charles Estle <bcestle@gmail.com>
2014-02-25 04:20:05 +0000

    FancierQuadTest and VertexData refactor

--- src/main/java/org/badassgame/sandbox/quad/VertexData.java
+++ src/main/java/org/badassgame/sandbox/quad/VertexData.java
@@ -4,7 +4,7 @@ public class VertexData {
     // Vertex data
     private float[] xyzw = new float[] {0f, 0f, 0f, 1f};
     private float[] rgba = new float[] {1f, 1f, 1f, 1f};
-    private float[] st = new float[] {0f, 0f};
+//    private float[] st = new float[] {0f, 0f};
 
     // The amount of bytes an element has
     public static final int elementBytes = 4;
@@ -12,12 +12,12 @@ public class VertexData {
     // Elements per parameter
     public static final int positionElementCount = 4;
     public static final int colorElementCount = 4;
-    public static final int textureElementCount = 2;
+//    public static final int textureElementCount = 2;
 
     // Bytes per parameter
     public static final int positionBytesCount = positionElementCount * elementBytes;
     public static final int colorByteCount = colorElementCount * elementBytes;
-    public static final int textureByteCount = textureElementCount * elementBytes;
+//    public static final int textureByteCount = textureElementCount * elementBytes;
 
     // Byte offsets per parameter
     public static final int positionByteOffset = 0;
@@ -26,10 +26,10 @@ public class VertexData {
 
     // The amount of elements that a vertex has
     public static final int elementCount = positionElementCount +
-            colorElementCount + textureElementCount;
+            colorElementCount;// + textureElementCount;
     // The size of a vertex in bytes, like in C/C++: sizeof(Vertex)
-    public static final int stride = positionBytesCount + colorByteCount +
-            textureByteCount;
+    public static final int stride = positionBytesCount + colorByteCount;// +
+            //textureByteCount;
 
     // Setters
     public void setXYZ(float x, float y, float z) {
@@ -40,9 +40,9 @@ public class VertexData {
         this.setRGBA(r, g, b, 1f);
     }
 
-    public void setST(float s, float t) {
-        this.st = new float[] {s, t};
-    }
+//    public void setST(float s, float t) {
+//        this.st = new float[] {s, t};
+//    }
 
     public void setXYZW(float x, float y, float z, float w) {
         this.xyzw = new float[] {x, y, z, w};
@@ -66,10 +66,10 @@ public class VertexData {
         out[i++] = this.rgba[0];
         out[i++] = this.rgba[1];
         out[i++] = this.rgba[2];
-        out[i++] = this.rgba[3];
+        out[i] = this.rgba[3];
         // Insert ST elements
-        out[i++] = this.st[0];
-        out[i++] = this.st[1];
+//        out[i++] = this.st[0];
+//        out[i++] = this.st[1];
 
         return out;
     }
@@ -90,7 +90,7 @@ public class VertexData {
         return new float[] {this.rgba[0], this.rgba[1], this.rgba[2]};
     }
 
-    public float[] getST() {
-        return new float[] {this.st[0], this.st[1]};
-    }
+//    public float[] getST() {
+//        return new float[] {this.st[0], this.st[1]};
+//    }
 }
